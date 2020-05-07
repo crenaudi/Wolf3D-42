9a78c33ff7102d75114f4814e3f3408e334c4fb9
camille renaudin <camillerenaudin@MBPdecamille2.home>
2020-05-07 13:26:01 +0200

    mlx_destroy.c

--- includes/wolf.h
+++ includes/wolf.h
@@ -142,5 +142,6 @@ t_vecf2			get_newpos(t_vecf2 dir, t_vecf2 coord, t_map map);
 int				move_player(t_player *plr, t_vec2 dir, t_map map, float speed);
 void			clean(t_env *e);
 void			clean_tab(int **map, int y);
+int				mlx_destroy(t_xvar *mlx);
 
 #endif
